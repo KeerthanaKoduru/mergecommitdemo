d160c7821a690372fe4dcace18bce758d8473eba
shubham096k <2021ugee096@nitjsr.ac.in>
2025-09-10 06:54:37 +0000

    Added Multiply method

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 class Calculator:
-  def __init__(self,a,b):
-    self.a=a
-    self.b=b
-    
+   def __init__(self, a, b):
+       self.a = a
+       self.b = b
+ 
+   def multiply(self):
+       return self.a * self.b
